199c5b98c75acee772fff693d6c246e18572609c
Selim Poladov <gegerout06@gmail.com>
2024-12-04 14:56:02 +0300

    first commit

---
+++ matrix.py
@@ -0,0 +1,35 @@
+class Matrix:
+    def __init__(self, n, m, matrix): # n - количество строк, m - количество столбцов
+        self.n = n
+        self.m = m
+        self.data = [] # данные в разреженном формате
+        self.indices = [] # индексы столбцов элементов data
+        self.ind_ptr = [] # индексы начала каждой строки в массиве data
+
+        # преобразование матрицы в разреженную
+        for i in range(n):
+            flag = False
+            for j in range(m):
+                if matrix[i][j] != 0:
+                    self.data.append(matrix[i][j])
+                    self.indices.append(j)
+                    if not flag:
+                        self.ind_ptr.append(self.data.index(matrix[i][j]))
+                    flag = True
+
+        self.ind_ptr.append(len(self.data))
+
+    # получение элемента разреженной матрицы по заданным индексам
+    def get_element(self, i, j):
+        for ind in range(len(self.data)):
+            if self.indices[ind] == j and self.ind_ptr[i] <= ind < self.ind_ptr[i + 1]:
+                return self.data[ind]
+        else:
+            return 0
+
+    # получение следа (с использованием get_element)
+    def get_trace(self):
+        if self.n == self.m:
+            return sum([self.get_element(i, i) for i in range(self.n)])
+        else:
+            return 0
---
+++ tests/test_matrix.py
@@ -0,0 +1,62 @@
+import unittest
+from matrix import Matrix  # Предполагаем, что класс Matrix находится в файле matrix.py
+
+
+class TestMatrix(unittest.TestCase):
+
+    def test_init(self):
+        matrix_data = [
+            [3, 0, 2],
+            [0, 1, 0],
+            [0, 0, 5]
+        ]
+        matrix = Matrix(3, 3, matrix_data)
+
+        # Проверяем, что после инициализации данные в разреженном формате соответствуют ожиданиям
+        expected_data = [3, 2, 1, 5]
+        expected_indices = [0, 2, 1, 2]
+        expected_ind_ptr = [0, 2, 3, 4]
+
+        self.assertEqual(matrix.data, expected_data)
+        self.assertEqual(matrix.indices, expected_indices)
+        self.assertEqual(matrix.ind_ptr, expected_ind_ptr)
+
+    def test_get_element(self):
+        matrix_data = [
+            [3, 0, 2],
+            [0, 1, 0],
+            [0, 0, 5]
+        ]
+        matrix = Matrix(3, 3, matrix_data)
+
+        # Проверяем извлечение элементов
+        self.assertEqual(matrix.get_element(0, 0), 3)
+        self.assertEqual(matrix.get_element(0, 2), 2)
+        self.assertEqual(matrix.get_element(1, 1), 1)
+        self.assertEqual(matrix.get_element(2, 2), 5)
+
+        # Проверяем элемент, которого нет в матрице (ноль)
+        self.assertEqual(matrix.get_element(1, 2), 0)
+        self.assertEqual(matrix.get_element(0, 1), 0)
+
+    def test_get_trace(self):
+        # Тест для квадратной матрицы
+        matrix_data = [
+            [3, 0, 2],
+            [0, 1, 0],
+            [0, 0, 5]
+        ]
+        matrix = Matrix(3, 3, matrix_data)
+        self.assertEqual(matrix.get_trace(), 9)  # 3 + 1 + 5 = 9
+
+        # Тест для неквадратной матрицы
+        matrix_data_non_square = [
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+        matrix_non_square = Matrix(2, 3, matrix_data_non_square)
+        self.assertEqual(matrix_non_square.get_trace(), 0)  # Неквадратная матрица не может иметь след
+
+
+if __name__ == '__main__':
+    unittest.main()
